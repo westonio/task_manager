296c17abd74061b492b8be0c000ee2d9280e75e0
Weston Schutt <wtschutt@gmail.com>
2023-06-28 12:09:23 -0600

    MVC: created task objects and ability to add records to db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,18 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["Task 1", "Task 2","Task 3"]
+    @tasks = Task.all
   end
 
   def new
-    
   end
+
+  def create
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to '/tasks'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   
   get "/tasks", to: "tasks#index"
   get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
 end
